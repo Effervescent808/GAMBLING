cde9a96b7e4d8288dbe7e9b3dd78e4fe47056ffd
Effervescent808 <HaufschildTyson@gmail.com>
2025-05-05 01:08:44 -0600

    Add files via upload

---
+++ Casino_Blackjack.py
@@ -0,0 +1,131 @@
+import random
+import sys
+import re
+
+#var for loosing 
+var = 0
+
+#get balance
+with open('account_balance.txt', 'rb') as file:
+    balance = ''.join(re.findall(r'\d+', str(file.readlines())))
+
+print()
+print("I love BlackJack")
+print()
+
+#set aces list
+ace = 'ace'
+aces = []
+
+#rename cards
+def card_name(value):
+    if value == 11:
+        return "Jack"
+    elif value == 12:
+        return "Queen"
+    elif value == 13:
+        return "King"
+    elif value == 1:
+        aces.append(ace)
+        return "Ace"
+    else:
+        return value
+
+#balance change function
+def balance_change(bal_change):
+    n_balance = (bal_change + (int(balance) - bet_amount))
+    temp = ('Balance =', n_balance)
+    write = str(temp).replace("'",'').replace(',','').replace('(','').replace(')','')
+    with open('account_balance.txt', 'w') as file:
+        file.write(write)
+
+#Get bet amount
+while True:
+    try:
+        print('current balance is: ',balance)
+        bet_amount = int(input("bet amount?: "))
+        if bet_amount <= int(balance):
+            break
+        else:
+            print('Not enough balance')
+    except ValueError:
+        print("Please enter a number")
+
+
+#set cards 1 and 2
+Card1 = random.randint(1, 13)
+Card2 = random.randint(1, 13)
+
+Card1_name = card_name(Card1)
+Card2_name = card_name(Card2)
+
+Card1_value = 10 if Card1 > 10 else Card1
+Card1_value = 11 if Card1 == 1 else Card1_value
+Card2_value = 10 if Card2 > 10 else Card2
+Card2_value = 11 if Card2 == 1 else Card2_value
+
+player_total = Card1_value + Card2_value
+
+print(f"First Card: {Card1_name}")
+print(f"Second Card: {Card2_name}")
+print()
+print(f"Player Total: {player_total}")
+print()
+
+
+Card3=''
+
+#play BLACKJACK
+while player_total < 99:
+
+    hos = input("Hit or Stay?: ")
+
+    if hos.lower() == "hit":
+        Card3 = random.randint(1,13)
+        Card3_name = card_name(Card3)
+        Card3_value = 10 if Card3 > 10 else Card3
+        Card3_value = 11 if Card3 == 1 else Card3_value
+        player_total += Card3_value
+
+        if player_total > 21 and len(aces) > 0:
+            player_total -= 10
+            aces.pop(0)
+        elif player_total > 21 and len(aces) == 0:
+            print()
+            print("New Card: ",Card3_name)
+            print("New Total: ",player_total)
+            print()
+            print("You Bust")
+            break
+
+        print()
+        print("New Card: ",Card3_name)
+        print("New Total: ",player_total)
+        print()
+    else:
+        break
+
+dealer = random.randint(17,23)
+
+print()
+print("The dealer had: ",dealer)
+print()
+money = (bet_amount * 2)
+if player_total > 21 and dealer > 21:
+    print("Push!")
+elif dealer > 21:
+    print("You Win!")
+    print('Winnings:', bet_amount * 2)
+    balance_change(money)
+    print("New Balance:",(int(balance) + bet_amount))
+elif player_total == dealer:
+    print("Push!")
+elif player_total > dealer:
+    print('You Win!')
+    print('Winnings:', bet_amount * 2)
+    balance_change(money)
+    print("New Balance:",(int(balance) + bet_amount))
+else:
+    print("You Lose :(")
+    balance_change(var)
+    print("New Balance:",(int(balance) - bet_amount))
---
+++ Casino_Roulette.py
@@ -0,0 +1,175 @@
+import random
+import time
+import re
+
+#get ball spin
+ball = random.randint(0,1)
+
+#var for loosing
+var = 0
+
+#lists for each number
+green = [0]
+red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+
+#Read balance file
+with open('account_balance.txt', 'rb') as file:
+    balance = ''.join(re.findall(r'\d+', str(file.readlines())))
+
+#Write to balance file
+def balance_change(bal_change):
+    n_balance = (bal_change + (int(balance) - bet_amount))
+    temp = ('Balance =', n_balance)
+    write = str(temp).replace("'",'').replace(',','').replace('(','').replace(')','')
+    with open('account_balance.txt', 'w') as file:
+        file.write(write)
+
+#find if even
+def isEven(number):
+    remainder = number % 2
+    if remainder > 0:
+        return False
+    else:
+        return True
+
+def spin(a):
+    print("Spinning...")
+    time.sleep(int(a))
+    print(".")
+    time.sleep(int(a))
+    print(".")
+    time.sleep(int(a))
+    print(".")
+    time.sleep(int(a))
+
+#get bet amount
+while True:
+    try:
+        print('current balance is: ',balance)
+        bet_amount = int(input("bet amount?: "))
+        if bet_amount <= int(balance):
+            break
+        else:
+            print('Not enough balance')
+    except ValueError:
+        print("Please enter a number")
+
+while True:
+
+    bet = input('bet on?: ').lower()
+
+    if bet.isdigit() and 0 < int(bet) < 37:
+        bet = int(bet)
+        spin(1)
+        print('Ball landed on:', ball)
+        if bet == ball:
+            print('You Win!')
+            print('Winnings:', bet_amount * 35)
+            money = (bet_amount * 35)
+            balance_change(money)
+            print("New Balance:",((int(balance) - bet_amount) + money))
+            break
+        else:
+            print('You Loose :(')
+            balance_change(var)
+            print("New Balance:",(int(balance) - bet_amount))
+            break
+    else:
+
+        #match case for choosing bets
+        match bet:
+            #Red Case
+            case 'red':
+                money = (bet_amount * 2)
+                spin(1)
+                print('Ball landed on:', ball)
+                if ball in red:
+                    print('You Win!')
+                    print('Winnings: ', bet_amount * 2)
+                    balance_change(money)
+                    print("New Balance:",((int(balance) - bet_amount) + money))
+                    break
+                else:
+                    print('You Loose :(')
+                    balance_change(var)
+                    print("New Balance:",(int(balance) - bet_amount))
+
+                    break
+            #Black Case
+            case 'black':
+                money = (bet_amount * 2)
+                spin(1)
+                print('Ball landed on:', ball)
+                if ball in black:
+                    print('You Win!')
+                    print('Winnings: ', bet_amount * 2)
+                    balance_change(money)
+                    print("New Balance:",((int(balance) - bet_amount) + money))
+                    break
+                else:
+                    print('You Loose :(')
+                    balance_change(var)
+                    print("New Balance:",(int(balance) - bet_amount))
+                    break
+            #Even Case
+            case 'even':
+                money = (bet_amount * 2)
+                spin(1)
+                print('Ball landed on:', ball)
+                if isEven(ball) == True and ball > 0:
+                    print('You Win!')
+                    print('Winnings:', bet_amount * 2)
+                    balance_change(money)
+                    print("New Balance:",((int(balance) - bet_amount) + money))
+                    break
+                elif isEven(ball) == True and ball == 0:
+                    print('You Loose :(')
+                    balance_change(var)
+                    print("New Balance:",(int(balance) - bet_amount))
+                    break
+                else:
+                    print('You Loose :(')
+                    balance_change(var)
+                    print("New Balance:",(int(balance) - bet_amount))
+                    break
+            #Odd Case
+            case 'odd':
+                money = (bet_amount * 2)
+                spin(1)
+                print('Ball landed on:', ball)
+                if isEven(ball) == False:
+                    print('You Win!')
+                    print('Winnings:', bet_amount * 2)
+                    balance_change(money)
+                    print("New Balance:",((int(balance) - bet_amount) + money))
+                    break
+                elif isEven(ball) == True and ball == 0:
+                    print('You Loose :(')
+                    balance_change(var)
+                    print("New Balance:",(int(balance) - bet_amount))
+                    break
+                else:
+                    print('You Loose :(')
+                    balance_change(var)
+                    print("New Balance:",(int(balance) - bet_amount))
+                    break
+            #"0" Case
+            case '0':
+                money = (bet_amount * 35)
+                spin(1)
+                print('Ball landed on:', ball)
+                if ball == 0:
+                    print('You Win!')
+                    print('Winnings:', bet_amount * 35)
+                    balance_change(money)
+                    print("New Balance:",((int(balance) - bet_amount) + money))
+                    break
+                else:
+                    print('You Loose :(')
+                    balance_change(var)
+                    print("New Balance:",(int(balance) - bet_amount))
+                    break
+            #"Not an Input" Case
+            case _:
+                print("unrecognized input")
